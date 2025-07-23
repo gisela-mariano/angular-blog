a0ca91b593b741b2c7d4d894ff18ea105605c5eb
Gisela Mariano <gisela-mariano@hotmail.com>
2025-07-23 12:11:10 -0300

    feat(ads-card): creates ads-card component

---
+++ src/app/components/molecules/ads-card/ads-card.ts
@@ -0,0 +1,19 @@
+import { CTA, TagTheme } from '@/types';
+import { Component, input, ViewEncapsulation } from '@angular/core';
+import { Button } from '@components/atoms/button/button';
+import { Tag } from '@components/atoms/tag/tag';
+
+@Component({
+  selector: 'app-ads-card',
+  imports: [Tag, Button],
+  templateUrl: './ads-card.html',
+  styleUrl: './ads-card.scss',
+  encapsulation: ViewEncapsulation.None,
+})
+export class AdsCard {
+  TagTheme = TagTheme;
+
+  title = input.required<string>();
+  subtitle = input.required<string>();
+  cta = input.required<CTA>();
+}
