deb38bc34d27f3df65f93ca494b0ef072b513ced
Gisela Mariano <gisela-mariano@hotmail.com>
2025-07-25 14:39:10 -0300

    feat(home): refactor the structure of the home page, adding articles section and adjusting card styles

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -1,6 +1,13 @@
+import { SectionTitle } from '@/app/components/atoms/section-title/section-title';
+import { FeaturedArticles } from '@/app/components/organisms/featured-articles/featured-articles';
 import { TagTheme } from '@/types';
 import { DatePipe } from '@angular/common';
-import { Component, CUSTOM_ELEMENTS_SCHEMA, signal } from '@angular/core';
+import {
+  Component,
+  CUSTOM_ELEMENTS_SCHEMA,
+  signal,
+  ViewEncapsulation,
+} from '@angular/core';
 import { AdsCard } from '@components/molecules/ads-card/ads-card';
 import { CarouselCard } from '@components/molecules/carousel-card/carousel-card';
 import { HeaderMenu } from '@components/molecules/header-menu/header-menu';
@@ -8,10 +15,19 @@ import { MainCard } from '@components/molecules/main-card/main-card';
 
 @Component({
   selector: 'app-home',
-  imports: [HeaderMenu, MainCard, DatePipe, AdsCard, CarouselCard],
+  imports: [
+    HeaderMenu,
+    MainCard,
+    DatePipe,
+    AdsCard,
+    CarouselCard,
+    SectionTitle,
+    FeaturedArticles,
+  ],
   templateUrl: './home.html',
   styleUrl: './home.scss',
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
+  encapsulation: ViewEncapsulation.None,
 })
 export class Home {
   TagTheme = TagTheme;
